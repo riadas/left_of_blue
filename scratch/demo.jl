3e814db5a46c6f32b314094e7d27388e6f15e9f0
riadas <riadas@mit.edu>
2025-04-02 19:17:26 -0400

    renamed DISTANCE to DEPTH, and added facing direction to intrinsic left/right definitions

--- scratch/demo.jl
+++ scratch/demo.jl
@@ -2,20 +2,20 @@
 abstract type Location end
 
 # --- LoB PROBLEM SPECIFICATION ---
-@enum DISTANCE close=5 mid=10 far=15
-Base.:(+)(a::DISTANCE, b::DISTANCE) = Int(a) + Int(b)
-Base.:(-)(a::DISTANCE, b::DISTANCE) = Int(a) - Int(b)
-Base.:(*)(a::DISTANCE, b::DISTANCE) = Int(a) * Int(b)
-Base.:(/)(a::DISTANCE, b::DISTANCE) = Int(a) / Int(b)
+@enum DEPTH close=5 mid=10 far=15
+Base.:(+)(a::DEPTH, b::DEPTH) = Int(a) + Int(b)
+Base.:(-)(a::DEPTH, b::DEPTH) = Int(a) - Int(b)
+Base.:(*)(a::DEPTH, b::DEPTH) = Int(a) * Int(b)
+Base.:(/)(a::DEPTH, b::DEPTH) = Int(a) / Int(b)
 
 @enum COLOR white blue
 
-struct Wall <: Location
-    distance::DISTANCE
+mutable struct Wall <: Location
+    depth::DEPTH
     color::COLOR
 end
 
-struct Corner <: Location
+mutable struct Corner <: Location
     wall1::Wall
     wall2::Wall
 end
@@ -46,14 +46,14 @@ function at(wall::Wall, color::COLOR)
 end
 
 # --- DEVELOPMENTAL STAGE 3 ---
-# LoB: intrinsic "left" language learned; not meaningful when viewer can rotate, e.g. LoB environments
-function left(location::Union{Wall, Corner})
-    true
+# LoB: intrinsic "left" language learned
+function left(location::Corner, facing::DEPTH)
+    corner.wall2.depth == facing
 end
 
-# LoB: intrinsic "right" language learned; not meaningful when viewer can rotate, e.g. LoB environments
-function right(location::Union{Wall, Corner})
-    true
+# LoB: intrinsic "right" language learned
+function right(location::Corner, facing::DEPTH)
+    corner.wall1.depth == facing
 end
 
 # spatial lang: intrinsic "left" language learned; not meaningful when viewer can rotate, e.g. LoB environments
@@ -93,24 +93,25 @@ wall1 = Wall(close, white)
 wall2 = Wall(far, blue)
 wall3 = Wall(close, white)
 wall4 = Wall(far, white)
-walls = [wall1, wall2, wall3, wall4]
 
 corner1 = Corner(wall1, wall2) # corner to the left of the blue wall
 corner2 = Corner(wall2, wall3)
 corner3 = Corner(wall3, wall4)
 corner4 = Corner(wall4, wall1)
-corners = [corner1, corner2, corner3, corner4]
 
-locations = [walls..., corners...]
+# left-to-right ordered list of walls and corners
+locations = [wall1, corner1, wall2, corner2, wall3, corner3, wall4, corner4] 
+
 scene = Scene(locations, corner1)
 
 # --- SPATIAL MEMORY REPRESENTATIONS ---
 # possible spatial memory representations (type check is auto-added, only second argument to && is user-generated)
-program1 = location -> location isa Corner && location.wall1.distance / location.wall2.distance > 1 # at a corner with this macroscopic geometry
-program2 = location -> location isa Wall && location.distance == close # at the center of a close wall
-program3 = location -> location isa Wall && location.distance == far # at the center of a far wall
+program1 = location -> location isa Corner && location.wall1.depth / location.wall2.depth > 1 # at a corner with this macroscopic geometry
+program2 = location -> location isa Wall && location.depth == close # at the center of a close wall
+program3 = location -> location isa Wall && location.depth == far # at the center of a far wall
 program4 = location -> location isa Wall && at(location, blue) # at the center of the blue wall
-program5 = location -> location isa Corner && left(location, blue) # left of the blue wall
+program5 = location -> location isa Corner && left(location, close) # to *my* left, when facing the close wall 
+program6 = location -> location isa Corner && left(location, blue) # to the left of the blue wall
 
 locations_to_search = filter(program1, scene.locations)
 searched = rand(locations_to_search) # uniform sampling over filtered search locations
