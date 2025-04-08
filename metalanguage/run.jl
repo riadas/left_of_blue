33a6db2b61eea4ed75c5494b11543de12369694d
riadas <riadas@mit.edu>
2025-04-08 17:55:50 -0400

    bug fixes

--- metalanguage/run.jl
+++ metalanguage/run.jl
@@ -16,27 +16,52 @@ left_of_function_spot = Function("left_of", ["location1_arg", "location2_arg"], 
 my_left_function_whole = Function("my_left", ["half_arg"], [Half], "")
 left_of_function_whole = Function("left_of", ["half1_arg", "half2_arg"], [Half, Half], "")
 
-ordered_function_sigs = [at_function, 
-                         my_left_function, 
-                         my_left_function_spot, 
+my_right_function = Function("my_right", ["location_arg", "depth_arg"], [Corner, DEPTH], "")
+right_of_function = Function("right_of", ["location_arg", "color_arg"], [Corner, COLOR], "")
+
+my_right_function_spot = Function("my_right", ["location_arg"], [Spot], "")
+right_of_function_spot = Function("right_of", ["location1_arg", "location2_arg"], [Spot, Spot], "")
+
+my_right_function_whole = Function("my_right", ["half_arg"], [Half], "")
+right_of_function_whole = Function("right_of", ["half1_arg", "half2_arg"], [Half, Half], "")
+
+left_of_function_wall = Function("left_of", ["location_arg", "color_arg"], [Wall, COLOR], "")
+right_of_function_wall = Function("right_of", ["location_arg", "color_arg"], [Wall, COLOR], "")
+
+ordered_function_sigs = [
+                         at_function, 
+                         my_left_function,
+                         my_right_function,
+                         my_left_function_spot,
+                         my_right_function_spot, 
                          my_left_function_whole,
+                         my_right_function_whole,
                          left_of_function, 
+                         right_of_function,
                          left_of_function_spot,
-                         left_of_function_whole] # , left_of_function
+                         right_of_function_spot,
+                         left_of_function_whole,
+                         right_of_function_whole,
+                         left_of_function_wall,
+                         right_of_function_wall
+                        ] 
 
 # ordered_function_sigs = [my_left_function_whole, left_of_function_whole]
-
-synthesized_semantics = []
+categories = [[Wall, Corner, DEPTH, COLOR], [Spot], [Half, Whole]]
+synthesized_semantics = Dict(map(x -> x => [], 1:length(categories)))
+prev_best_scores = Dict(map(x -> x => -1.0, 1:length(categories)))
 for function_sig in ordered_function_sigs
+    category_assignment = findall(c -> intersect(function_sig.arg_types, c) != [], categories)[1]
+
     possible_semantics = []
     for i in 1:1000
         semantics = generate_semantics(function_sig, base_semantics)
         push!(possible_semantics, semantics)
     end
     possible_semantics = unique(possible_semantics)
-
+    # println(possible_semantics)
     # exclude function definitions that have been used before
-    possible_semantics = filter(x -> !(x in synthesized_semantics), possible_semantics)
+    possible_semantics = filter(x -> !(x in synthesized_semantics[category_assignment]), possible_semantics)
 
     # exclude function definitions that don't use all of the input arguments
     possible_semantics = filter(x -> foldl(&, map(a -> occursin(a, x), function_sig.arg_names), init=true), possible_semantics)
@@ -66,7 +91,17 @@ for function_sig in ordered_function_sigs
         # if performance is higher than base language, save that program and its score
 
         scores = Dict()
-        for config_name in readdir("spatial_config/configs")
+
+        config_names = readdir("spatial_config/configs")
+        if category_assignment == 1 # LoB experiments
+            config_names = filter(x -> occursin("room", x), config_names)
+        elseif category_assignment == 2 # spatial lang. experiments
+            config_names = filter(x -> occursin("spatial", x), config_names)
+        elseif category_assignment == 3 # red-green experiments
+            config_names = filter(x -> occursin("green", x), config_names)
+        end
+
+        for config_name in config_names
             # all non-control input spatial problems
             if !occursin("no_blue", config_name) && !occursin(".DS_Store", config_name)
                 # println(config_name)
@@ -115,8 +150,8 @@ for function_sig in ordered_function_sigs
                     if !(scene.prize isa Whole)
                         push!(results, filter(x, scene.locations))
                     else
-                        shift = (scene.prize.red.x + scene.prize.green.x)/2
-                        shifted_prize = Whole(Half(scene.prize.green.x - shift), Half(scene.prize.red.x + shift), scene.prize.diagonal)
+                        shift = (scene.prize.coral.x + scene.prize.green.x)/2
+                        shifted_prize = Whole(Half(scene.prize.green.x - shift), Half(scene.prize.coral.x - shift), scene.prize.diagonal)
                         new_locations = [shifted_prize]
                         res = filter(x, new_locations)
 
@@ -133,10 +168,11 @@ for function_sig in ordered_function_sigs
                 end
 
                 if scene.prize isa Whole 
+                    # println("----- config name: $(config_name)")
                     idxs = findall(x -> !isnothing(x), results)
                     programs = map(i -> programs[i], idxs)
                     results = map(i -> results[i], idxs)
-
+                    
                     # println("programs")
                     # println(programs)
                     # println("results")
@@ -146,15 +182,24 @@ for function_sig in ordered_function_sigs
                         scores[config_name] = 1/3
                     else
                         programs_and_results = sort([zip(programs, results)...], by=tup -> length(tup[1]))
+                        # for tup in programs_and_results 
+                        #     println("program and result")
+                        #     println(tup[1])
+                        #     println(tup[2])
+                        # end
                         program, res = programs_and_results[end]
                         # println(program)
                         # println(res)
+                        # println("score")
                         if scene.prize in res 
                             scores[config_name] = 1/length(res)
+                            # println(1/length(res))
                         elseif length(res) == []
                             scores[config_name] = 1/3
+                            # println(1/3)
                         else
                             scores[config_name] = 0
+                            # println(0)
                         end 
                     end
 
@@ -197,9 +242,14 @@ for function_sig in ordered_function_sigs
             best_definition = definition
             best_total_score = total_score
             best_full_results = scores
-            println("new best!")
+            println("----- new best!")
             println(definition)
             println(total_score)
+            # println(scores)
+        else
+            # println("----- discarding...")
+            # println(definition)
+            # println(total_score)
         end 
 
     end
@@ -207,21 +257,28 @@ for function_sig in ordered_function_sigs
     # println(best_full_results)
 
     # update final semantics and syntax with new function definition
-    function_sig.definition = best_definition
-    new_function_definition_str = format_new_function_string(function_sig)
-    println("new function definition")
-    println(new_function_definition_str)
+    if best_total_score > prev_best_scores[category_assignment]
+        prev_best_scores[category_assignment] = best_total_score
+        function_sig.definition = best_definition
+        new_function_definition_str = format_new_function_string(function_sig)
+        println("new function definition")
+        println(new_function_definition_str)
+        
+        push!(synthesized_semantics[category_assignment], best_definition)
     
-    push!(synthesized_semantics, best_definition)
-
-    global base_semantics_str = join([base_semantics_str, new_function_definition_str], "\n")
-
-    open("metalanguage/final_semantics.jl", "w+") do f 
-        write(f, base_semantics_str)
+        global base_semantics_str = join([base_semantics_str, new_function_definition_str], "\n")
+    
+        open("metalanguage/final_semantics.jl", "w+") do f 
+            write(f, base_semantics_str)
+        end
+    
+        # update semantics cfg 
+        global base_semantics = update_semantics_cfg(base_semantics, function_sig)
+    else
+        # remove function from syntax CFG 
+        updated_syntax = update_syntax_cfg(base_syntax, function_sig, remove=true)
     end
 
-    # update semantics cfg 
-    global base_semantics = update_semantics_cfg(base_semantics, function_sig)
 end
 
 # TODO: 
